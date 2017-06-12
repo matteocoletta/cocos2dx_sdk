fa92ae1712479592af041df957fff84906fb41d0
Obaied <abdullah.obaied@gmail.com>
2017-06-12 15:23:12 +0200

    Add scripts dir

---
+++ scripts/cocos_testing_android_ci.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Get the current directory (/scripts/ directory)
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Traverse up to get to the root directory
+SDK_DIR="$(dirname "$SCRIPTS_DIR")"
+EXAMPLE_DIR=example
+RED='\033[0;31m' # Red color
+GREEN='\033[0;32m' # Green color
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}>>> Removing Android JARs${NC}"
+rm -rfv Android/*
+
+echo -e "${GREEN}>>> Building Android JARs${NC}"
+(cd ext/android; ./build.sh)
+(cd ext/android; ./build_testing.sh)
+
+echo -e "${GREEN}>>> Building the Android ci testing JAR file ${NC}"
+cd ${SDK_DIR}
+cp -rf Android/* ${EXAMPLE_DIR}/proj.android-studio/app/libs
+
+echo -e "${GREEN}>>> Running cocos project ${NC}"
+#cocos run -p android --android.studio
