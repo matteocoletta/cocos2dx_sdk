f82adf2c5f3b2f59d7b46da1ab7a3b82849bb286
Obaied <abdullah.obaied@gmail.com>
2017-06-13 11:44:35 +0200

    Amending test script

--- scripts/cocos_testing_android_ci.sh
+++ scripts/cocos_testing_android_ci.sh
@@ -21,4 +21,5 @@ cd ${SDK_DIR}
 cp -rf Android/* ${EXAMPLE_DIR}/proj.android-studio/app/libs
 
 echo -e "${GREEN}>>> Running cocos project ${NC}"
-cocos run -p android --android.studio
+cd ${EXAMPLE_DIR}
+cocos run -p android --android-studio
